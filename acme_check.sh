1a99956fba2e3b4d56e24d1da1b290a62f1b441c
lmr <41105878+lushchan@users.noreply.github.com>
2021-10-15 23:16:37 +0300

    Update acme_check.sh

--- acme_check.sh
+++ acme_check.sh
@@ -5,8 +5,13 @@ CurDate=$(date +"%s")
 #CurDate="1639394914"
 #30 days in epoch format 
 Month="2678400"
-AcmeDate=$(/root/.acme.sh/acme.sh --list | awk '{doms=$1" "$3; print doms}' | sed 's/,/\n/g' | sed 's/\s/\n/g' | sed '/^no$/d' | tail -n +3)
-for i in $AcmeDate ; do
+#Path to file with non-acme domains
+ExternalFile="/usr/lib64/nagios/plugins/external"
+# Get domains from acme client
+AcmeList=$(/root/.acme.sh/acme.sh --list | awk '{doms=$1" "$3; print doms}' | sed 's/,/\n/g' | sed 's/\s/\n/g' | sed '/^no$/d' | tail -n +3)
+ExternalList=$(echo -e "\n" ; cat $ExternalFile)
+AcmeList+=$ExternalList
+for i in $AcmeList ; do
   ExprDate=($(echo | openssl s_client -showcerts -servername $i -connect $i:443 2>/dev/null | openssl x509 -inform pem -noout -text | egrep '(Not After : )' | sed 's/.*Not\ After\ \:\ //g' | { read gmt ; date -d "$gmt" +"%s" ; }))
 #Get date in epoch when certificate must be updated
   UpdDate=$(expr $ExprDate - $Month)
